4dbd1f985504062a5312fbb6d494f87c8e7236e0
Andre Joseph Cubeta <ajcubeta@gmail.com>
2011-09-12 16:29:57 +0800

    Upload test app

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+gem 'haml-rails'
+gem 'jquery-rails'
+gem "jquery-tmpl-rails"
+
+group :assets do
+  gem 'sass-rails', "~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :development do
+  # Faster development?
+  gem 'active_reload'
+
+  # irb
+  gem 'wirble'
+  gem 'hirb'
+  gem 'awesome_print'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqueryTemplateBlog::Application.config.secret_token = '1a4691900a4423bdfb694273a133b2affc5e53590f5acae32aa995bb976f405dff3659cc752138fccd7c119f301ce76399d10dfb5eeba2f29c89739c6b03ed4d'
